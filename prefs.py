a2c00e59df637c66116f246e5b1e30ab779f1aa7
ffsgfy <somefortera@gmail.com>
2019-12-25 20:47:25 +0500

    .

--- prefs.py
+++ prefs.py
@@ -1,9 +1,9 @@
 import random
 
-P_SPLIT = 1 / 2
+P_SPLIT = 1 / 2  # probability of splitting on each label
 P_INSERT_PLAIN = 1 / 5
 P_INSERT_JUMP = 1 / 8
-LABEL_TEMPLATE = f"_L{random.randint(2**16 - 1, 2**32 - 1):X}_{{:X}}"
+LABEL_TEMPLATE = f"_L{random.randrange(2**12, 2**32):X}_{{:X}}"
 
 
 def insert_plain():
